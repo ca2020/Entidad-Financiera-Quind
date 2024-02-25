36da8940a40dc83ccba434cfb2481d0efdc4e92c
carlos <broli2012_@hotmail.com>
2024-02-24 20:02:05 -0500

    Creacion de proyecto

---
+++ src/main/java/org/example/entidadfinancieraquind/EntidadFinancieraQuindApplication.java
@@ -0,0 +1,13 @@
+package org.example.entidadfinancieraquind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadFinancieraQuindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadFinancieraQuindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/entidadfinancieraquind/EntidadFinancieraQuindApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.entidadfinancieraquind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntidadFinancieraQuindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
